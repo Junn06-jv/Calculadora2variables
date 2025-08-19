141e5c0533192515a6d143d8811ea09d9fc98bb4
Junn <zjair12345@gmail.com>
2025-08-19 11:53:59 -0500

    Formateo

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Hello {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Escribe un numero:");
         int a = sc.nextInt();
@@ -9,29 +9,27 @@ public class Hello {
         int b = sc.nextInt();
         int opcion;
         do {
-                    formato();
-        opcion =sc.nextInt();
-        if (opcion == 1) {
-            System.out.println("El resultado de tu suma es:");
-            System.out.println(suma(a,b));
-        }else if (opcion ==2) {
-            System.out.println("El resultado de tu resta es:");
-            System.out.println(resta(a,b));
-        }else if (opcion ==3) {
-            System.out.println("El resultado de tu multiplicacion:");
-            System.out.println(multiplicacion(a,b));
-        }else if (opcion ==4) {
-            System.out.println("El resultado de tu division es:");
-            System.out.println(division(a,b));
-        }else if (opcion ==5) {
-            System.out.println("Saliendo del programa.");
-        }
-        else{
-            System.out.println("Escoje una opcion valida");
-        }
+            formato();
+            opcion = sc.nextInt();
+            if (opcion == 1) {
+                System.out.println("El resultado de tu suma es:");
+                System.out.println(suma(a, b));
+            } else if (opcion == 2) {
+                System.out.println("El resultado de tu resta es:");
+                System.out.println(resta(a, b));
+            } else if (opcion == 3) {
+                System.out.println("El resultado de tu multiplicacion:");
+                System.out.println(multiplicacion(a, b));
+            } else if (opcion == 4) {
+                System.out.println("El resultado de tu division es:");
+                System.out.println(division(a, b));
+            } else if (opcion == 5) {
+                System.out.println("Saliendo del programa.");
+            } else {
+                System.out.println("Escoje una opcion valida");
+            }
         } while (opcion != 5);
-        
-     
+
     }
 
     public static void formato() {
@@ -47,6 +45,7 @@ public class Hello {
         return num1 + num2;
 
     }
+
     public static int resta(int num1, int num2) {
         return num1 - num2;
     }
